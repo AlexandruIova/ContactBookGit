5db54356308e3c10c394ccfbffc665453205186f
AlexIova <iovaalexandru37@gmail.com>
2022-09-20 17:21:05 +0200

    Typo

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ public class Main {
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
     public static final String PHONE_NOT_EXIST = "Phone number does not exist.";
-    public static final String NO_SAME_PHONE = "All contacts have different phone numbers";
+    public static final String NO_SAME_PHONE = "All contacts have different phone numbers.";
     public static final String FOUND_SAME_PHONE = "There are contacts that share phone numbers.";
 
     public static void main(String[] args) {
